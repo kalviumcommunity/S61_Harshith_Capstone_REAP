00464e9d872a5f6bc167f07eaa11be38354a2b0c
Harshith200525 <lemniscatelooper@outlook.com>
2024-05-10 15:46:47 +0530

    put endpoint defined

---
+++ Backend/routes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.get('/', (req, res) => res.send('Hello World!'))
+
+router.post('/post', (req, res) => {
+  res.send(req.body)
+})
+router.put('/update/:id', (req, res) => {
+    const noteId = req.params.id;
+    const updateData = req.body;
+    res.send(updateData);
+});
+
+module.exports = router;
