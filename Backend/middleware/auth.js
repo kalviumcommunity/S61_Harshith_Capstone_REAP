aaa0206d816f60bcfb35fcfd20015db6e0fa9cce
Harshith Amaravathi <149362534+Harshith200525@users.noreply.github.com>
2024-11-26 17:39:58 +0530

    Update auth.js

--- Backend/middleware/auth.js
+++ Backend/middleware/auth.js
@@ -4,7 +4,7 @@ const auth = (req, res, next) => {
   try {
     const token = req.headers.authorization.replace('Bearer ', '');
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded;
+    req.user = { userId: decoded.id };
     next();
   } catch (err) {
     res.status(401).json({ message: 'Authentication failed!' });
